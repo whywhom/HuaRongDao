4346a0865295d9510bb9cac24f671ac43418b6d1
whywhom <whywhom@163.com>
2019-11-29 12:43:12 +1300

    更改功能

---
+++ app/src/main/java/com/whywhom/soft/huarongdao/ui/activity/GameActivity.java
@@ -0,0 +1,13 @@
+package com.whywhom.soft.huarongdao.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
---
+++ app/src/main/java/com/whywhom/soft/huarongdao/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.whywhom.soft.huarongdao.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
