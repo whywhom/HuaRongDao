4a3bbc62047af78b82a1dfa90fe12885b30e83fc
whywhom <whywhom@163.com>
2019-11-29 12:43:12 +1300

    更改功能

---
+++ app/src/main/java/com/whywhom/soft/huarongdao/ui/main/MainFragment.java
@@ -0,0 +1,34 @@
+package com.whywhom.soft.huarongdao.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.whywhom.soft.huarongdao.R;
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    private MainViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
